0d73c37da00f7be2b46e6d29cdd093e3869514e7
ikari shizuku <ikari2002pcs@icloud.com>
2024-11-08 16:05:25 +0900

    ハッカソンのスクショを表示

--- lib/main.dart
+++ lib/main.dart
@@ -116,13 +116,28 @@ class ProjectCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.symmetric(vertical: 8),
-      child: ListTile(
-        title: Text(project.title),
-        subtitle: Text(project.description),
-        trailing: const Icon(Icons.open_in_new),
+      child: InkWell(
         onTap: () {
           launchURL(hackathonProjectUrl);
         },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+              title: Text(project.title),
+              subtitle: Text(project.description),
+              trailing: const Icon(Icons.open_in_new),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                'assets/images/hackathon_app.png', 
+                height: 400, // 高さを調整
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
